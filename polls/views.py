dbee981a11858351c49d9d537415de89ee3d60da
Hemabh Kamboj <hkamboj2603199@gmail.com>
2018-09-05 00:44:25 +0530

    added form button

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question 
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
@@ -13,8 +13,8 @@ def detail(request, question_id):
         
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
